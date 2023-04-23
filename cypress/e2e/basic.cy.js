2a03394201b6c5202cf9819ccd650b0b29e96419
jeocool <131511301+jeocool@users.noreply.github.com>
2023-04-23 20:03:37 +0000

    FUCK

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,15 +2,8 @@ describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+  it('references the stupid sink thing', () => {
+    cy.get('h1')
+    .contains('Sink');
   })
 })
